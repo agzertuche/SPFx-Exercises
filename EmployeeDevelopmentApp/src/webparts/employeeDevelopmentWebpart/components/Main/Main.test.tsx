997162cb11656183b33262ecbfe25291ddc859b8
Arturo <agzertuche@hotmail.com>
2017-08-15 19:47:05 -0500

    tests fixed

--- EmployeeDevelopmentApp/src/webparts/employeeDevelopmentWebpart/components/Main/Main.test.tsx
+++ EmployeeDevelopmentApp/src/webparts/employeeDevelopmentWebpart/components/Main/Main.test.tsx
@@ -1,29 +1,16 @@
-// /// <reference types="mocha" />
+/// <reference types="mocha" />
 
-// import * as React from 'react';
-// import TestUtils from 'react-addons-test-utils'; 
-// import { assert, expect } from 'chai';
-// import { mount } from 'enzyme';
-// import Main from './Main';
+import * as React from 'react';
+import * as TestUtils from 'react-addons-test-utils'; 
+import { assert, expect } from 'chai';
+import { mount } from 'enzyme';
+import Main from './Main';
 
-// declare const sinon;
+declare const sinon;
  
-// describe('<Main />', () => {
-//   it('Should render something', () => {
-//     let component = ReactTestUtils.
-//   })
-  
-// });
-
-
-
-// // it('does foo', () => {
-//     let component = ReactTestUtils.renderIntoDocument(
-//       <ComponentName
-//         componentProps={props}}
-//         />
-//     );
-//       let renderedDOM = ReactDOM.findDOMNode(component as React.ReactInstance);
-//       let componentName = renderedDOM.querySelector('<unique selector>');
-//       expect(componentName).to.be.eq('foo', 'componentName was not foo');
-//   });
+describe('<Main />', () => {
+  it('Should render something', () => {
+    expect(0).to.be.greaterThan(0);
+    assert.ok(true);
+  });  
+});
